226bdfe8ef80f960c9188e24980196e440c2c9c2
SquishyStrawberry <therealrebuilder@gmail.com>
2015-05-09 19:46:28 +0200

    Made a better install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
 projectRoot=$(pwd)
+
+if hash python3; then
+    echo "Found Python3"
+else
+    echo "Python3 was not found, exiting..."
+    exit 1
+fi
+
+if [[ -e ${projectRoot}/requirements.txt ]]; then
+    echo "Found global requirements.txt in ${projectRoot}"
+    pip3 install -r requirements.txt
+else
+    echo "No global requirements.txt in ${projectRoot}"
+fi
+
 for directory in $( ls -Cd */ | egrep ".*?\s+" ); do
     cd ${projectRoot}/${directory}
-    for pyFile in $(ls -C *.py | egrep ".*?\s+"); do
-        if [[ pyFile -eq "setup.py" ]]; then
-            echo "Installing $projectRoot/$directory"
-            python3 setup.py install
-            break
-        fi
-    done
+    echo "Checking ${projectRoot}/${directory}"
+    if [[ -e ${projectRoot}/${directory}/setup.py ]]; then
+        echo "Installing ${projectRoot}/${directory}"
+        python3 setup.py install
+    else
+        echo "No setup.py in ${projectRoot}/${directory}"
+    fi
 done
