0585cf0f25e98851845a9563c794d299512652e8
SquishyStrawberry <therealrebuilder@gmail.com>
2015-05-24 04:23:38 +0200

    (Cloudjumper) Forgot to update version in the Cloudjumper class.

--- Cloudjumper/cloudjumper/cloudjumper.py
+++ Cloudjumper/cloudjumper/cloudjumper.py
@@ -28,7 +28,7 @@ class Cloudjumper(irc_helper.IRCHelper):
     excluded_nicknames = ("skullcrusher", "thornado")
     plugins = []
     cloudjumper_logger = logging.getLogger(__name__)
-    version = 2.0
+    version = 2.001
 
 
     def __init__(self, config_file, extra_settings={}):
