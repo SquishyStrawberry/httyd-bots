8dc2bcd98259ce13b12952074771d021063e56fb
SquishyStrawberry <therealrebuilder@gmail.com>
2015-05-30 00:05:50 +0200

    Added option to make a venv in install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
+# Exists on error.
+set -e
 projectRoot=$(pwd)
 
 if hash python3; then
     echo "Found Python3"
+    if hash pip3; then
+        echo "Found Pip3"
+    else
+        echo "Pip3 was not found, exiting..." >&2
+        exit 1
+    fi
 else
-    echo "Python3 was not found, exiting..."
+    echo "Python3 was not found, exiting..." >&2
     exit 1
 fi
+
+echo -ne "It is heavily reccomended you make a virtual enviorement. Do you want to make one? [y/n]\n> "
+read userChoice
+if [[ ${userChoice:0} != 'n' ]]; then
+    echo "Making venv... (this will take a while\!)"
+    python3 -m venv httyd_venv
+    source httyd_venv/bin/activate
+else
+    echo "The packages will be installed globally."
+fi
+
 
 # To make sure you get the new version.
 pip3 freeze | egrep "(?i)thornado|irc[-_]helper|cloudjumper" | xargs pip3 uninstall -y
