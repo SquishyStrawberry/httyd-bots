603ba061353284b2c82ab55e379d9b099411f560
SquishyStrawberry <therealrebuilder@gmail.com>
2015-05-29 18:22:58 +0200

    (Cloudjumper) Added modules function.

--- Cloudjumper/setup.py
+++ Cloudjumper/setup.py
@@ -15,7 +15,7 @@ packs = setuptools.find_packages()
 def setup():
     setuptools.setup(
         name="cloudjumper",
-        version=2.003,
+        version=2.004,
         description="A bot that can learn, attack, eat, etc... in IRC!",
         author="SquishyStrawberry",
         install_requires=reqs,
---
+++ Cloudjumper/standard_modules/listmodules.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+private_message = False
+name_needed = True
+
+def message_handler(bot, message, sender):
+    if bot.is_command("modules", message, name_needed):
+        modules = ", ".join(i.__name__ for i in bot.channel_commands + bot.private_commands)
+        bot.send_action(bot.get_message("list_modules").format(modules=modules))
+        return True
