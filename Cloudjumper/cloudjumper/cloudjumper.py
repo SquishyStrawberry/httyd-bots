8ca3d3a01b481c1f44073bfed9f6e789bfd45006
SquishyStrawberry <therealrebuilder@gmail.com>
2015-05-29 18:22:58 +0200

    set + set -> tuple + tuple

--- Cloudjumper/cloudjumper/cloudjumper.py
+++ Cloudjumper/cloudjumper/cloudjumper.py
@@ -28,7 +28,7 @@ class Cloudjumper(irc_helper.IRCHelper):
     excluded_nicknames = ("skullcrusher", "thornado")
     plugins = []
     cloudjumper_logger = logging.getLogger(__name__)
-    version = 2.002
+    version = 2.004
 
 
     def __init__(self, config_file, extra_settings={}):
